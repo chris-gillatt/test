307feb410ec72be29422c89a4b4e3a4746d3c428
Chris Gillatt <chris.gillatt@autotrader.co.uk>
2018-09-19 20:54:46 +0100

    waffle

--- gillatty.rb
+++ gillatty.rb
@@ -2,10 +2,11 @@ class Gillatty < Formula
   desc "Test"
   homepage "https://github.com/chris-gillatt"
   version "0.1"
+  sha256 "77d909d93a9fb03968ef12f366612ae21d7e3639cfa2fde6ff8c96387d44202f"
 
   url "https://github.com/chris-gillatt/test/blob/master/gillatty.sh", :using => :curl
 
   def install
-    bin.install "chris-gillatt/gillatty"
+    bin.install "gillatty.sh"
   end
 end
