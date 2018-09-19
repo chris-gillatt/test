e17f53991e303371645f363985060924b0ba4283
Chris Gillatt <chris.gillatt@autotrader.co.uk>
2018-09-19 20:57:39 +0100

    waffle

--- gillatty.rb
+++ gillatty.rb
@@ -2,7 +2,6 @@ class Gillatty < Formula
   desc "Test"
   homepage "https://github.com/chris-gillatt"
   version "0.1"
-  sha256 "b74dfee6d4b8350183d14e59d03d511721e7f0d6093ddf0d341347fa49231293"
 
   url "https://github.com/chris-gillatt/test/blob/master/gillatty.sh", :using => :curl
 
