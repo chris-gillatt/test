e5d338e09d485331e356110c2e7f62d59d1aa164
Chris Gillatt <chris.gillatt@autotrader.co.uk>
2018-09-19 21:45:31 +0100

    tests

--- gillatty.rb
+++ gillatty.rb
@@ -1,9 +1,13 @@
 class Gillatty < Formula
-  desc "Test"
+  desc "test bollocks"
   homepage "https://github.com/chris-gillatt"
-  version "0.1"
+  url "https://github.com/chris-gillatt/test/raw/master/archive/gillatty-0.0.1.tar.gz"
+  sha256 "a4b69610942aa4f3e2356342eaa9406ff90aec5399734a184865578f4dd8c9c2"
+  version "0.0.1"
 
-  url "https://github.com/chris-gillatt/test/blob/master/gillatty", :using => :curl
+  depends_on "curl"
+
+  bottle :unneeded
 
   def install
     bin.install "gillatty"
