5f71927ed5d2525c11a584c6136d16364bbece98
chris-gillatt <solex@hotmail.co.uk>
2021-11-28 17:25:34 +0000

    Push gillatty Release 0.0.1

--- gillatty.rb
+++ gillatty.rb
@@ -2,7 +2,7 @@ class Gillatty < Formula
   desc "Test"
   homepage "https://github.com/chris-gillatt/test"
   url "https://github.com/chris-gillatt/test/releases/download/0.0.1/gillatty-0.0.1.tar.gz"
-  sha256 "c71841e59bdc695d6b0a485d4a62d32d60831fe0ba3febf821be962d54d44d8e"
+  sha256 "abbf8fb483a1f11272481ef31899aeb2310f60aaf323b8f333594b4c8207e53b"
   version "0.0.1"
 
   depends_on "curl"
