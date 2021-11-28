69c69a763a97549a95a76eab940b590f9cc9a2e9
chris-gillatt <solex@hotmail.co.uk>
2021-11-28 16:54:28 +0000

    Push gillatty Release 0.0.19

--- gillatty.rb
+++ gillatty.rb
@@ -1,13 +1,13 @@
-class Dockerfix < Formula
-  desc "Convenient housekeeping for docker Mac users"
-  homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/releases/download/0.0.18/dockerfix-0.0.18.tar.gz"
+class Gillatty < Formula
+  desc "Test"
+  homepage "https://github.com/chris-gillatt/test"
+  url "https://github.com/chris-gillatt/test/releases/download/0.0.19/test-0.0.19.tar.gz"
   sha256 ""
-  version "0.0.18"
+  version "0.0.19"
 
   depends_on "curl"
 
   def install
-    bin.install "dockerfix"
+    bin.install "test"
   end
 end
